9c90cf0c34fa0181c9203aed9cc7b0ee2c85ff10
Austine The-Enigma <austine574@gmail.com>
2026-01-26 23:59:47 +0100

    feat(client): check

---
+++ client/src/lib/test.ts
@@ -0,0 +1,13 @@
+import { useUsers } from "../features/users/useUsers";
+
+export default function App() {
+  //   const { data, isLoading } = useUsers();
+  //   if (isLoading) return <p>Loading...</p>;
+  //   return (
+  //     <div>
+  //       {data.map((u: any) => (
+  //         <p key={u.id}>{u.email}</p>
+  //       ))}
+  //     </div>
+  //   );
+}
